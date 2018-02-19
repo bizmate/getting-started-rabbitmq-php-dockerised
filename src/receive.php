b291911caeede79f6c0efd69bae227c545e4c5fc
Diego Gullo <diego_gullo@bizmate.biz>
2018-02-19 13:47:18 +0000

    Added persistance and prefetch limited to 1 per worker, including named docker volume for RabbitMQ data

--- src/receive.php
+++ src/receive.php
@@ -12,19 +12,20 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('hello', false, true, false, false);
 
 echo ' [*] Waiting for messages. To exit press CTRL+C', "\n";
 
 $callback = function($msg) {
     $waitSeconds = rand(15,30);
-    echo " [x] Host: " . getenv('HOSTNAME') ." Waiting: " . $waitSeconds . "  Received: ", $msg->body, "\n";
+    echo " [x] Host: " . getenv('HOSTNAME') ." Waiting: " . $waitSeconds . " seconds. Received msg: ", $msg->body, "\n";
     //sleep(substr_count($msg->body, '.'));
     sleep($waitSeconds);
     echo " [x] Done", "\n";
     $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
 };
 
+$channel->basic_qos(null, 1, null);
 $channel->basic_consume('hello', '', false, false, false, false, $callback);
 
 while(count($channel->callbacks)) {
--- src/send.php
+++ src/send.php
@@ -15,9 +15,11 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('rabbitmq', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-$channel->queue_declare('hello', false, false, false, false);
+$channel->queue_declare('hello', false, true, false, false);
 
-$msg = new AMQPMessage('Hello World!');
+$msg = new AMQPMessage('Hello World!',
+    array('delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT)
+);
 $channel->basic_publish($msg, '', 'hello');
 
 echo " [x] Sent 'Hello World!'\n";
